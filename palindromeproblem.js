882022d82e79aa7af62fe953e7a0cba9147131df
ankit <ankraz123@gmail.com>
2022-11-02 12:38:58 +0530

    first commit

---
+++ palindromeproblem.js
@@ -0,0 +1,9 @@
+let str="naman";
+let new_str="";
+for(i=str.length-1; i>=0; i--){
+    new_str+=str[i];
+}if(new_str==str[i]){
+    console.log("palindrome");
+}else{
+    console.log("not palindrome");
+}
