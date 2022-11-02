2c9d2d0119422da91b3912254872f1ed008f7d6c
amankverma001 <kumarvermaaman001@gmail.com>
2022-11-02 12:50:13 +0530

    changes commited

--- palindromeproblem.js
+++ palindromeproblem.js
@@ -2,8 +2,8 @@ let str="naman";
 let new_str="";
 for(i=str.length-1; i>=0; i--){
     new_str+=str[i];
-}if(new_str==str[i]){
-    console.log("palindrome");
+}if(new_str==str){
+    console.log("palindrome string");
 }else{
-    console.log("not palindrome");
+    console.log("not a palindrome string");
 }
--- primeproblem.js
+++ primeproblem.js
@@ -2,10 +2,10 @@ let num=13;
 let count=0;
 for(i=1; i<=num; i++){
     if(num%i==0){
-      count--;
+      count++;
     }
 }if(count==2){
-    console.log("it is prime");
+    console.log("it is prime no");
 }else{
-    console.log("it is not prime");
+    console.log("it is not prime no");
 }
